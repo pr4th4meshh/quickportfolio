42a9ec2f6ef4e7cf2f51e92201a6281217c47955
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-12-14 19:15:12 +0530

    add: copy to clipboard & share

--- src/app/(routes)/[portfolioUsername]/_components/SharePresssenceButton.tsx
+++ src/app/(routes)/[portfolioUsername]/_components/SharePresssenceButton.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react"
 
 const SharePresssenceButton = () => {
+  const copyToClipboard = () => {
+    const textArea = document.createElement("textarea")
+    textArea.value = window.location.href
+    document.body.appendChild(textArea)
+    textArea.select()
+    document.execCommand("copy")
+    textArea.remove()
+  }
   return (
-    <div className="absolute sm:fixed transform -rotate-90  bg-green-600 text-white origin-top-left top-[350px] z-50 left-0 py-1 px-2 sm:px-4 rounded-b-lg sm:py-1 text-sm sm:text-lg tracking-wide border-t-0">
-        Share your Presssence
+    <div
+      onClick={copyToClipboard}
+      className="absolute sm:fixed transform -rotate-90  bg-green-600 text-white origin-top-left top-[350px] z-50 left-0 py-1 px-2 sm:px-4 rounded-b-lg sm:py-1 text-sm sm:text-lg tracking-wide border-t-0 cursor-pointer"
+    >
+      Share your Presssence
     </div>
   )
 }
