8ca34b456fbf1956c3646ea351abec023fbf1052
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-12-17 19:30:27 +0530

    add: edit cover image

--- src/app/(routes)/[portfolioUsername]/_components/PortfolioProjects.tsx
+++ src/app/(routes)/[portfolioUsername]/_components/PortfolioProjects.tsx
@@ -13,6 +13,8 @@ import {
 import ProjectThumbnail from "../../../../../public/project_thumbnail.jpg"
 import PrimaryButton from "@/components/ui/primary-button"
 import EditButton from "./EditButton"
+import { storage } from "@/lib/firebase"
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage"
 
 interface IProject {
   id?: string
@@ -20,26 +22,90 @@ interface IProject {
   description: string
   link: string
   timeline: string
+  coverImage?: string
 }
 
-const PortfolioProjects = ({ initialProjects }: any) => {
+const PortfolioProjects = ({
+  initialProjects,
+}: {
+  initialProjects: { projects: IProject[]; userId: string }
+}) => {
   const [isEditing, setIsEditing] = useState(false)
   const [projects, setProjects] = useState<IProject[]>(initialProjects.projects)
+  const [imagePreview, setImagePreview] = useState<string | null>(null)
+  const [uploading, setUploading] = useState(false)
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const [imageUploaded, setImageUploaded] = useState(false)
+  const [uploadProgress, setUploadProgress] = useState(0)
   const { data: session } = useSession()
   const params = useParams()
 
-  const handleInputChange = (index: number, field: keyof IProject, value: string) => {
+  let progress = 0
+
+  const handleInputChange = (
+    index: number,
+    field: keyof IProject,
+    value: string
+  ) => {
     const updatedProjects = [...projects]
     updatedProjects[index] = { ...updatedProjects[index], [field]: value }
     setProjects(updatedProjects)
   }
 
   const handleAddProject = () => {
-    setProjects([...projects, { title: "", description: "", link: "", timeline: "" }])
+    setProjects([
+      ...projects,
+      { title: "", description: "", link: "", timeline: "" },
+    ])
   }
 
   const handleRemoveProject = (index: number) => {
     setProjects(projects.filter((_, i) => i !== index))
+  }
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const file = e.target.files ? e.target.files[0] : null
+    if (file) {
+      // Preview the selected image
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string)
+        setSelectedFile(file)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  const handleUploadImage = async (index: number) => {
+    if (!selectedFile) return
+
+    setUploading(true)
+    const storageRef = ref(storage, `/projects/${selectedFile.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile)
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setUploadProgress(progress)
+      },
+      (error) => {
+        console.error("Upload failed:", error)
+        setUploading(false)
+      },
+      async () => {
+        // Once the image is uploaded, get the download URL
+        setImageUploaded(true)
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref)
+        const updatedProjects = [...projects]
+        updatedProjects[index] = {
+          ...updatedProjects[index],
+          coverImage: downloadURL,
+        }
+        setProjects(updatedProjects)
+        setUploading(false)
+      }
+    )
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -71,121 +137,191 @@ const PortfolioProjects = ({ initialProjects }: any) => {
   const isAGrid = projects.length > 1
 
   return (
-  projects.length > 0 && (
-    <div className="py-32 container mx-auto px-4">
-    <h1 className="text-center pb-10 text-3xl uppercase">Projects</h1>
+    projects.length > 0 && (
+      <div className="py-32 container mx-auto px-4">
+        <h1 className="text-center pb-10 text-2xl uppercase">Projects</h1>
 
-    {isEditing ? (
-      <form onSubmit={handleSubmit} className="space-y-6">
-        {projects.map((project, index) => (
-          <div key={index} className="border border-gray-300 rounded-lg p-4 space-y-4">
-            <div>
-              <label htmlFor={`title-${index}`} className="block text-sm font-medium py-1">Title</label>
-              <input
-                type="text"
-                id={`title-${index}`}
-                value={project.title}
-                onChange={(e) => handleInputChange(index, "title", e.target.value)}
-                // className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            className="flex-grow w-full p-2 border border-gray-300 rounded-md"
+        {isEditing ? (
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className="border border-gray-300 rounded-lg p-4 space-y-4"
+              >
+                <div>
+                  <label
+                    htmlFor={`title-${index}`}
+                    className="block text-sm font-medium py-1"
+                  >
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    id={`title-${index}`}
+                    value={project.title}
+                    onChange={(e) =>
+                      handleInputChange(index, "title", e.target.value)
+                    }
+                    className="flex-grow w-full p-2 border border-gray-300 rounded-md"
+                  />
+                </div>
 
-              />
-            </div>
-            <div>
-              <label htmlFor={`description-${index}`} className="block text-sm font-medium py-1">Description</label>
-              <textarea
-                id={`description-${index}`}
-                value={project.description}
-                onChange={(e) => handleInputChange(index, "description", e.target.value)}
-                rows={3}
-                // className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            className="flex-grow p-2 border border-gray-300 rounded-md w-full"
+                <div>
+                  <label
+                    htmlFor={`description-${index}`}
+                    className="block text-sm font-medium py-1"
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    id={`description-${index}`}
+                    value={project.description}
+                    onChange={(e) =>
+                      handleInputChange(index, "description", e.target.value)
+                    }
+                    rows={3}
+                    className="flex-grow p-2 border border-gray-300 rounded-md w-full"
+                  />
+                </div>
 
-              />
+                <div>
+                  <label
+                    htmlFor={`link-${index}`}
+                    className="block text-sm font-medium py-1"
+                  >
+                    Link
+                  </label>
+                  <input
+                    type="url"
+                    id={`link-${index}`}
+                    value={project.link}
+                    onChange={(e) =>
+                      handleInputChange(index, "link", e.target.value)
+                    }
+                    className="flex-grow p-2 border border-gray-300 rounded-md w-full"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor={`timeline-${index}`}
+                    className="block text-sm font-medium py-1"
+                  >
+                    Timeline
+                  </label>
+                  <input
+                    type="date"
+                    id={`timeline-${index}`}
+                    value={project.timeline}
+                    onChange={(e) =>
+                      handleInputChange(index, "timeline", e.target.value)
+                    }
+                    className="flex-grow p-2 border border-gray-300 rounded-md w-full"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor={`image-${index}`} className="block text-sm font-medium py-1">
+                    Project Image
+                  </label>
+                  <input
+                    type="file"
+                    id={`image-${index}`}
+                    accept="image/*"
+                    onChange={(e) => handleImageChange(e, index)}
+                    className="block w-full p-2 border border-gray-300 rounded-md"
+                  />
+                  {imagePreview && (
+                    <div className="mt-4">
+                      <Image
+                        src={imagePreview}
+                        alt="Image Preview"
+                        className="h-32 w-32 object-cover border rounded-xl dark:border-white border-black"
+                        height={100}
+                        width={100}
+                      />
+                    </div>
+                  )}
+                  {uploading && <p>Uploading... {uploadProgress}%</p>}
+                  {imageUploaded && <p className="text-blue-400 mt-2">Image uploaded successfully! Click Save changes to apply the changes.</p>}
+                  {!uploading && imagePreview && (
+                    <button
+                      type="button"
+                      disabled={!selectedFile || imageUploaded}
+                      onClick={() => handleUploadImage(index)}
+                      className="mt-2 inline-flex items-center disabled:opacity-80 px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-700"
+                    >
+                      Upload Image
+                    </button>
+                  )}
+                </div>
+
+                <button
+                  type="button"
+                  onClick={() => handleRemoveProject(index)}
+                  className="mt-2 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                >
+                  <FaTrash className="mr-2" /> Remove Project
+                </button>
+              </div>
+            ))}
+            <div className="flex justify-between">
+              <button
+                type="button"
+                onClick={handleAddProject}
+                className="flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-green-500"
+              >
+                <FaPlus className="mr-2" /> Add Project
+              </button>
+              <div className="flex space-x-2">
+                <PrimaryButton
+                  title="Cancel"
+                  onClick={() => setIsEditing(false)}
+                  className="bg-red-500 text-white"
+                />
+                <PrimaryButton title="Save Changes" type="submit" />
+              </div>
             </div>
-            <div>
-              <label htmlFor={`link-${index}`} className="block text-sm font-mediumpy-1">Link</label>
-              <input
-                type="url"
-                id={`link-${index}`}
-                value={project.link}
-                onChange={(e) => handleInputChange(index, "link", e.target.value)}
-                // className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            className="flex-grow p-2 border border-gray-300 rounded-md w-full"
-              />
-            </div>
-            <div>
-              <label htmlFor={`timeline-${index}`} className="block text-sm font-medium py-1">Timeline</label>
-              <input
-                type="date"
-                id={`timeline-${index}`}
-                value={project.timeline}
-                onChange={(e) => handleInputChange(index, "timeline", e.target.value)}
-                // className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            className="flex-grow p-2 border border-gray-300 rounded-md w-full"
-              />
-            </div>
-            <button
-              type="button"
-              onClick={() => handleRemoveProject(index)}
-              className="mt-2 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+          </form>
+        ) : (
+          <div className="relative">
+            <div
+              className={`${
+                isAGrid ? "grid grid-cols-1 sm:grid-cols-3 gap-4" : "flex flex-col space-y-4"
+              }`}
             >
-              <FaTrash className="mr-2" /> Remove Project
-            </button>
-          </div>
-        ))}
-        <div className="flex justify-between">
-          <button
-            type="button"
-            onClick={handleAddProject}
-            className="flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-green-500"
-          >
-            <FaPlus className="mr-2" /> Add Project
-          </button>
-          <div className="flex space-x-2">
-            <PrimaryButton
-              title="Cancel"
-              onClick={() => setIsEditing(false)}
-              className="bg-red-500 text-white"
-            />
-            <PrimaryButton title="Save Changes" type="submit" />
-          </div>
-        </div>
-      </form>
-    ) : (
-      <div className="relative">
-        <div className={`${isAGrid ? "grid grid-cols-1 sm:grid-cols-3 gap-4" : "flex flex-col space-y-4"}`}>
-          {projects.map((project: IProject, index: number) => (
-            <Card key={index}>
-              <Image
-                src={ProjectThumbnail}
-                alt={project.title}
-                width={200}
-                height={200}
-                className="border-2 border-background h-[250px] w-full object-cover"
-                quality={100}
+              {projects.map((project: IProject, index: number) => (
+                <Card key={index}>
+                  <Image
+                    src={project.coverImage || ProjectThumbnail}
+                    alt={project.title}
+                    width={200}
+                    height={200}
+                    className="border-2 border-background h-[250px] w-full object-cover"
+                    quality={100}
+                  />
+                  <CardTitle>{project.title}</CardTitle>
+                  <CardDescription>{project.description}</CardDescription>
+                  <CardDescription className="flex flex-row items-center">
+                    <FaClock className="mr-2" />
+                    {new Date(project.timeline).toDateString()}
+                  </CardDescription>
+                  <CardActions>
+                    <ProjectLink link={project.link} />
+                  </CardActions>
+                </Card>
+              ))}
+            </div>
+            {session?.user?.id === initialProjects.userId && (
+              <EditButton
+                className="float-right mt-5"
+                onClick={() => setIsEditing(true)}
               />
-              <CardTitle>{project.title}</CardTitle>
-              <CardDescription>{project.description}</CardDescription>
-              <CardDescription className="flex flex-row items-center">
-                <FaClock className="mr-2" /> {new Date(project.timeline).toDateString()}
-              </CardDescription>
-              <CardActions>
-                <ProjectLink link={project.link} />
-              </CardActions>
-            </Card>
-          ))}
-        </div>
-        {session?.user?.id === initialProjects.userId && (
-          <EditButton
-            className="float-right mt-5"
-            onClick={() => setIsEditing(true)}
-          />
+            )}
+          </div>
         )}
       </div>
-    )}
-  </div>
-  )
+    )
   )
 }
 
